ba48e4b876f780e5ae1e1d51773990d299692d50
Panuwit Onkaew <panuwit8315@gmail.com>
2021-10-21 09:36:27 +0700

    AddSound&UIEndGame&UIHintP2

---
+++ UnityProject/Assets/Scripts/DebugCtrl.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCtrl : MonoBehaviour
+{
+    [SerializeField] GameObject controlPanel;
+    [SerializeField] GameObject content;
+    [SerializeField] GameObject debugTemp;
+    [SerializeField] Button debugBtn;
+    [SerializeField] Button clearBtn;
+    [SerializeField] Button enterBtn;
+    [SerializeField] InputField inputField;
+
+    static DebugCtrl instance;
+    List<GameObject> currentDebug = new List<GameObject>();
+    bool canUseInput = false;
+
+    Game game;
+
+    public static DebugCtrl GetDebug() { return instance; }
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        OnBack();
+        clearBtn.onClick.AddListener(() =>
+        {
+            Clear();
+        });
+        enterBtn.onClick.AddListener(() =>
+        {
+            OnEnterBtn();
+        });
+        game = Game.GetInstance();
+    }
+
+    void OnBack()
+    {
+        controlPanel.SetActive(false);
+        debugBtn.transform.Find("Text").GetComponent<Text>().text = "Debug";
+        debugBtn?.onClick.RemoveAllListeners();
+        debugBtn.onClick.AddListener(() =>
+        {
+            OnDebug();
+        });
+    } 
+
+    void OnDebug()
+    {
+        controlPanel.SetActive(true);
+        debugBtn.transform.Find("Text").GetComponent<Text>().text = "กลับ";
+        debugBtn?.onClick.RemoveAllListeners();
+        debugBtn?.onClick.AddListener(() =>
+        {
+            OnBack();
+        });
+    }
+
+    public void Log(object message,bool isJeng = false)
+    {
+        GameObject g = Instantiate(debugTemp, content.transform);
+        g.SetActive(true);
+        g.GetComponent<Text>().text = message.ToString();
+        currentDebug.Add(g);
+        if (isJeng)
+        {
+            Button b = g.AddComponent<Button>();
+            g.GetComponent<Text>().color = Color.red;
+            g.GetComponent<Text>().fontStyle = FontStyle.Bold;
+            b.onClick.AddListener(() =>
+            {
+                Application.OpenURL("https://www.instagram.com/jkp_jeng/");                
+            });
+        }
+        if (currentDebug.Count > 50) 
+        {
+            Destroy(currentDebug[0]);
+            currentDebug.RemoveAt(0);
+        }      
+        
+        Debug.Log(message);
+    }
+
+    void Clear()
+    {
+        foreach(GameObject g in currentDebug) Destroy(g);
+        currentDebug.Clear();
+    }
+
+    void OnEnterBtn()
+    {
+        if (string.IsNullOrEmpty(inputField.text)) return;
+
+        string input = inputField.text;
+        inputField.text = "";
+
+        if (!canUseInput)
+        {
+            if (input == "jkp_jeng" || input == "Jkp_jeng" || input == "JKP_JENG") 
+            {  
+                canUseInput = true;
+                Log("คุณมีสิทธิ์ใช้คำสั่งต่างๆแล้ว");
+                Log(">>>>กดตรงข้อความนี้ครับ<<<<",true);
+            }
+            else
+            {
+                Log("คุณไม่มีสิทธิ์ในใช้คำสั่งต่างๆ");
+            }
+            return;
+        }
+
+        string[] inputs = input.Split(' ');
+        if ((inputs[0] != "dev") && (inputs[0] != "Dev"))
+        {
+            Log("เราไม่รู้จักคำสั่งของคุณ");
+            return;
+        }
+        if(inputs.Length < 2)
+        {
+            Log("เราไม่รู้จักคำสั่งของคุณ");
+            return;
+        }
+
+        string command = inputs[1];
+        if(command == "next" || command == "Next")
+        {
+            if(!(game.scene == SceneState.GAMEPLAY))
+            {
+                Log("ต้องสั่งในหน้า GamePlay");
+                return;
+            }
+            if (inputs.Length < 3)
+            {
+                Log("เราไม่รู้จักคำสั่งของคุณ");
+                return;
+            }
+            int value;
+            if(!Int32.TryParse(inputs[2], out value))
+            {
+                Log("คำสั่ง Next ต้องสั่งแบบนี้ [Dev next 31]" + value);
+                return;
+            }
+
+            if (value > 0)
+            {
+                game.fridgeSpawner.SetFridgeCount(value-1);
+                UIManager.GetUI().UIGamePlay().SetActiveAllItem();
+                Log("ตู้เย็นตู้ต่อไป จะเป็นตู้ที่ " + value+" และปลดล็อกไอเทมทั้งหมด");
+                return;
+            }
+            else
+            {
+                Log("คำสั่งนี้ต้องสั่งด้วยตัวเลขที่มากกว่า0");
+                return;
+            }
+        }
+        else if (command == "time" || command == "Time")
+        {
+            if (inputs.Length < 3)
+            {
+                Log("เราไม่รู้จักคำสั่งของคุณ");
+                return;
+            }
+            if(inputs[2] == "5")
+            {
+                if(game.scene == SceneState.GAMEPLAY)
+                {
+                    game.timer.SetTime(5);
+                    Log("ลดเวลาเหลือ 5 วินาที");
+                    return;
+                }
+                else
+                {
+                    Log("ต้องสั่งในหน้า GamePlay");
+                    return;
+                }               
+            }
+            else
+            {
+                Log("เราไม่รู้จักคำสั่งของคุณ");
+                return;
+            }
+        }
+        else
+        {
+            Log("เราไม่รู้จักคำสั่งของคุณ");
+            return;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum SfxClipName {COMBO, PICKOUTCORRECT01, PICKOUTCORRECT02, PICKOUTFAIL01, PICKOUTFAIL02, TIMEOUT, FRIDGECOMPLETE, CLICK01, CLICK02, ENDGAME }
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager GetInstance() { return instance; }
+    AudioSource bgmTrack01, bgmTrack02;
+    AudioSource sfxTrack;
+
+    public AudioMixerGroup bgmMixer;
+    public AudioMixerGroup sfxMixer;
+
+    bool isPlayingBgmTrack01;
+    [SerializeField] AudioClip defaultClip;
+    [SerializeField] AudioClip[] clipsSFX;
+    [SerializeField] AudioClip[] clipsTest;
+    [SerializeField] int countClipsTest = 0;
+
+    int volumeSfxOn = 0;
+    [SerializeField]int volumeBgmOn = 0;
+    int volumeOff = -80;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        bgmTrack01 = gameObject.AddComponent<AudioSource>();
+        bgmTrack02 = gameObject.AddComponent<AudioSource>();
+        sfxTrack = gameObject.AddComponent<AudioSource>();
+        
+        bgmTrack01.outputAudioMixerGroup = bgmMixer; bgmTrack01.loop = true;
+        bgmTrack02.outputAudioMixerGroup = bgmMixer; bgmTrack02.loop = true;
+        sfxTrack.outputAudioMixerGroup = sfxMixer;
+
+        isPlayingBgmTrack01 = true;
+        ChangeBGMClip(defaultClip);
+
+        if (!IsOnBGMSettingInSave()) SwitchOnBGMMixer(false);
+        else SwitchOnBGMMixer(true);
+        if (!IsOnSFXSettingInSave()) SwitchOnSFXMixer(false);
+        else SwitchOnSFXMixer(true);
+    }
+
+    public void ChangeBGMClip(AudioClip newClip)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeTrack(newClip));
+
+        isPlayingBgmTrack01 = !isPlayingBgmTrack01;
+    }
+
+    private IEnumerator FadeTrack(AudioClip clip)
+    {
+        float timeToFade = 1.25f;
+        float timeElapsed = 0;
+
+        if (isPlayingBgmTrack01)
+        {
+            bgmTrack02.clip = clip;
+            bgmTrack02.Play();
+
+            while (timeElapsed < timeToFade)
+            {
+                bgmTrack02.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                bgmTrack01.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            bgmTrack01.Stop();
+        }
+        else
+        {
+            bgmTrack01.clip = clip;
+            bgmTrack01.Play();
+
+            while (timeElapsed < timeToFade)
+            {
+                bgmTrack01.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                bgmTrack02.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            bgmTrack02.Stop();
+        }
+    }
+
+    public void SwitchOnBGMMixer(bool isOn)
+    {
+        int volume;
+        if (isOn) volume = volumeBgmOn;
+        else volume = volumeOff;
+
+        bgmMixer.audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetInt("BgmVolume", volume);
+        DebugCtrl.GetDebug().Log("SoundMN-->Set BGM:" + volume);
+    }
+
+    public void SwitchOnSFXMixer(bool isOn)
+    {
+        int volume;
+        if (isOn) volume = volumeSfxOn;
+        else volume = volumeOff;
+
+        sfxMixer.audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetInt("SfxVolume", volume);
+        DebugCtrl.GetDebug().Log("SoundMN-->Set SFX:" + volume);
+    }
+
+    public bool IsOnBGMSettingInSave()
+    {
+        if (PlayerPrefs.GetInt("BgmVolume", volumeBgmOn) == volumeBgmOn) return true;
+        else return false;
+    }
+
+    public bool IsOnSFXSettingInSave()
+    {
+        if (PlayerPrefs.GetInt("SfxVolume", volumeSfxOn) == volumeSfxOn) return true;
+        else return false;
+    }
+
+    public void PlaySFXOneShot(SfxClipName clipName)
+    {
+        sfxTrack.PlayOneShot(clipsSFX[(int)clipName]);
+    }
+
+    //For Test Only
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            countClipsTest++;
+            if (countClipsTest >= clipsTest.Length) countClipsTest = 0;
+            ChangeBGMClip(clipsTest[countClipsTest]);
+        }
+    }
+    //End For Test Only
+}
